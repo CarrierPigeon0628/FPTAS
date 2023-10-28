7e4f0eae01134c27c837d8a50921da036ec25d8a
CarrierPigeon0628 <83458347+CarrierPigeon0628@users.noreply.github.com>
2023-10-29 01:44:48 +0800

    Add files via upload

--- FPTAS.py
+++ FPTAS.py
@@ -4,7 +4,7 @@ import time
 e = 0.01  # epsilon
 
 # Generate a sample.
-# N = np.random.randint(3) + 1  # number of groups
+# N = np.random.randint(100) + 1  # number of groups
 N = 20  # for testing
 V = np.random.random() * (10 ** np.random.randint(1, 3))  # knapsack capacity (volume)
 S = []  # numbers of items of each group
@@ -12,7 +12,7 @@ v = []  # values of each item
 w = []  # weights of each item
 x = []  # solution
 for i in range(N):
-    # S.append(np.random.randint(3) + 1)
+    # S.append(np.random.randint(100) + 1)
     S.append(20)  # for testing
     vi = []
     wi = []
@@ -30,14 +30,10 @@ print("w =", w)
 
 # FPTAS
 st = time.perf_counter()
-# Find the max value.
 P = max([max(v[i]) for i in range(N)])
-# Scale all the values by K = e * P / N, and convert them to integers.
 v = [[int(v[i][j] * N / e / P) for j in range(S[i])] for i in range(N)]
-# dp[j] records the min weight to get the value of j.
 dp = np.array([0] + [np.inf] * (N * int(N / e)))
-# sol[i][j] records which item of the i-th group to pick to get the value of j with the min weight.
-sol = np.zeros((N, N * int(N / e)), dtype=np.int32)
+sol = np.zeros((N, N * int(N / e) + 1), dtype=np.int32)
 for i in range(N):
     for j in range(N * int(N / e), -1, -1):
         for k in range(S[i]):

c338cb70cb025a49b6a574a5502050f3ef0ac13c
CarrierPigeon0628 <83458347+CarrierPigeon0628@users.noreply.github.com>
2023-10-31 11:59:07 +0800

    Add files via upload

--- FPTAS.py
+++ FPTAS.py
@@ -22,32 +22,36 @@ for i in range(N):
     v.append(vi)
     w.append(wi)
     x.append([0] * S[-1])
-# print("N =", N)
-# print("V =", V)
-# print("S =", S)
-# print("v =", v)
-# print("w =", w)
+print("N =", N)
+print("V =", V)
+print("S =", S)
+print("v =", v)
+print("w =", w)
 
 # FPTAS
 st = time.perf_counter()
 P = max([max(v[i]) for i in range(N)])
-v = [[int(v[i][j] * N / e / P) for j in range(S[i])] for i in range(N)]
-dp = np.array([0] + [np.inf] * (N * int(N / e)))
-sol = np.zeros((N, N * int(N / e) + 1), dtype=np.int32)
+K = e * P / N
+v = [[int(v[i][j] / K) for j in range(S[i])] for i in range(N)]
+vm = N * int(N / e) + 1
+dp = np.inf * np.ones((N + 1, vm))
+dp[0, 0] = 0
+sol = np.zeros((N, vm), dtype="int")
 for i in range(N):
-    for j in range(N * int(N / e), -1, -1):
-        for k in range(S[i]):
-            if j >= v[i][k] and dp[j - v[i][k]] + w[i][k] < dp[j]:
-                dp[j] = dp[j - v[i][k]] + w[i][k]
-                sol[i][j] = k + 1
-for j in range(N * int(N / e), -1, -1):
-    if dp[j] <= V:
-        # print("opt =", j * e * P / N)
+    dp[i + 1, 0: vm] = np.copy(dp[i, 0: vm])
+    for k in range(S[i]):
+        v_temp = v[i][k]
+        w_temp = w[i][k]
+        sol[i, v_temp: vm][dp[i, 0: vm - v_temp] + w_temp < dp[i + 1, v_temp: vm]] = k + 1
+        dp[i + 1, v_temp: vm] = np.minimum(dp[i + 1, v_temp: vm], dp[i, 0: vm - v_temp] + w_temp)
+for j in range(vm - 1, -1, -1):
+    if dp[N, j] <= V:
+        print("opt =", K * j)
         break
 for i in range(N - 1, -1, -1):
-    if sol[i][j] > 0:
-        x[i][sol[i][j] - 1] = 1
-        j -= v[i][sol[i][j] - 1]
-# print("x =", x)
+    if sol[i, j] > 0:
+        x[i][sol[i, j] - 1] = 1
+        j -= v[i][sol[i, j] - 1]
+print("x =", x)
 et = time.perf_counter()
 print(et - st)
